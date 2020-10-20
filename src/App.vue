a1b067eb4e37ed70cf818fc23bed435c1789835e
Sisovattra <sisovattrayi@gmail.com>
2020-10-20 20:59:44 +0700

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,257 @@
+<template>
+  <div>
+    <v-card
+      width="550"     
+    >
+    <div><HelloWorld/></div>
+        <div class="horizontal_slider">
+          <div class="slider_container">
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+            <div class="item">   
+              <img src="./assets/bouquet-lavande-illustration-realiste_1284-9237.jpg">
+              <div style="text-align: center">
+                Lavender
+                <br>Price: $3.00
+              </div>
+            </div>
+          </div>
+        </div>
+    </v-card>
+      </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  h1 {
+    font-size: 5vw;
+    text-transform: uppercase;
+    margin: 15px 0;
+  }
+
+  .horizontal_slider{
+    display:block;
+    width: 100%;
+    overflow-x: scroll;
+    padding: 20px;
+    box-sizing: border-box;
+    background-color: #eee;
+  }
+
+  .horizontal_slider::-webkit-scrollbar{
+    display: none;
+  }
+
+  .horizontal_slider{
+    -ms-overflow-style: none;
+  }
+
+  .slider_container {
+    display: block;
+    white-space: nowrap;
+  }
+
+  .item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  .item img {
+    border-radius: 50%;
+    width: 225px;
+    height: 220px;
+    object-fit: cover;
+  }
+</style>
